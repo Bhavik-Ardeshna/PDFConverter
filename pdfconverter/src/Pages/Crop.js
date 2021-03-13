18c5fc6f3bee20dfe28ea7fd472879975018a598
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-14 00:10:52 +0530

    crop download align

--- pdfconverter/src/Pages/Crop.js
+++ pdfconverter/src/Pages/Crop.js
@@ -225,12 +225,14 @@ export const Crop = () => {
 
                                 </div>
                             </div>
-                            {returnFile ? (
-                                <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
-                                    type="submit" onClick={downloadFile}>
-                                    Download
-                                </button>) : null}
+                            <div className="my-12">
 
+                                {returnFile ? (
+                                    <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                        type="submit" onClick={downloadFile}>
+                                        Download
+                                    </button>) : null}
+                            </div>
                         </div>
                     </div>
                 </div>
