1712419727ba2b3b92ae3366f1e742019f9feca3
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-13 23:49:32 +0530

    added crop design

--- pdfconverter/src/Pages/Crop.js
+++ pdfconverter/src/Pages/Crop.js
@@ -140,8 +140,82 @@ export const Crop = () => {
         </li>
     ));
     return (
-        <div>
+        <>
+            <div className="backImg leading-normal text-indigo-400  bg-cover bg-fixed">
+                <div className="h-screen">
+                    <Navbar />
+                    <div className="w-full">
 
-        </div>
+                        <div class="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                            <div className=" flex flex-row">
+                                <div class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start ">
+
+                                    <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+
+                                        <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-100 via-pink-400 to-purple-700">
+                                            Crop
+                                     </span>
+
+                                    </h1>
+
+                                    <form method="POST" enctype="multipart/form-data">
+                                        <div className="container">
+                                            <div {...getRootProps({ style })}>
+                                                <input {...getInputProps()} />
+                                                <p className="my-4 text-3xl md:text-2xl mr-3 text-white opacity-75 font-bold leading-tight text-center md:text-left">Drag and drop some files here</p>
+                                                <div className='uploadbutton'>
+                                                    <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                                        type="submit" onClick={onSubmit}>
+                                                        Upload
+                                                    </button>
+                                                </div>
+
+                                            </div>
+                                            <aside>
+                                                <ul>{filesdata}</ul>
+                                            </aside>
+                                        </div >
+
+                                        <div className="mt-5">
+                                            <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                                type="submit" onClick={onSubmit}>
+                                                Upload
+                                            </button>
+                                        </div>
+                                    </form>
+                                    <div className="mt-5">
+                                        <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                            type="submit" onClick={onPreview}>
+                                            Preview
+                                        </button>
+                                    </div>
+
+                                </div>
+                                <div className="ml-64">
+
+
+                                    {flag ? (
+                                        <div className="ml-5">
+                                            <ReactCrop src={previewImg} crop={crop} onChange={newCrop => setCrop(newCrop)} />
+                                            <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                                type="submit" onClick={doCrop}>
+                                                Crop
+                                            </button>
+                                        </div>
+                                    ) : null}
+
+                                </div>
+                            </div>
+                            {returnFile ? (
+                                <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                    type="submit" onClick={downloadFile}>
+                                    Download
+                                </button>) : null}
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
