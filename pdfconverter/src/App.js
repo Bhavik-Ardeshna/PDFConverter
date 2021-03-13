e1c33440d239517933e269dffe6b886578e5d89f
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-13 12:05:03 +0530

    added router and pages js file

--- pdfconverter/src/App.js
+++ pdfconverter/src/App.js
@@ -1,10 +1,41 @@
+import React from "react";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import './App.css';
-
+import backImg from "./Static/image/header.png";
 function App() {
   return (
     <>
-      hello
+      <Router>
+        <Switch>
+
+          <Route path="/rotate">
+            {/* <Rotate /> */}
+          </Route>
+
+          <Route path="/crop">
+            {/* <Crop /> */}
+          </Route>
+
+          <Route path="/merge">
+            {/* <Merge /> */}
+          </Route>
+
+          <Route path="/split">
+            {/* <Split /> */}
+          </Route>
+
+          <Route path="/convert">
+            {/* <Convert /> */}
+          </Route>
+
+          <Route path="/">
+            {/* Home page */}
+          </Route>
+
+        </Switch>
+      </Router >
     </>
   );
 }
