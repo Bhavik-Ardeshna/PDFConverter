044a7d9d2916ae80c781fec8f58f2314c04d972a
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-13 23:28:54 +0530

    added merge functionality and convert backend

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -20,6 +20,116 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
+
+########################################################################################################################
+# Convert
+########################################################################################################################
+
+@app.route('/convert/download/<filename>', methods=['GET', 'POST'])
+def return_files_convert(filename):
+    file_path = './zip/'+filename
+    return send_file(file_path, as_attachment=True, attachment_filename='', cache_timeout=0)
+
+
+def convert_func(case, fname, pages=None):
+    if not pages:
+        pagenums = set()
+    else:
+        pagenums = set(pages)
+    manager = PDFResourceManager()
+    codec = 'utf-8'
+    caching = True
+
+    if case == 'text':
+        output = io.StringIO()
+        converter = TextConverter(
+            manager, output, codec=codec, laparams=LAParams())
+    if case == 'HTML':
+        output = io.BytesIO()
+        converter = HTMLConverter(
+            manager, output, codec=codec, laparams=LAParams())
+
+    interpreter = PDFPageInterpreter(manager, converter)
+    infile = open(fname, 'rb')
+
+    for page in PDFPage.get_pages(infile, pagenums, caching=caching, check_extractable=True):
+        interpreter.process_page(page)
+
+    convertedPDF = output.getvalue()
+
+    infile.close()
+    converter.close()
+    output.close()
+    return convertedPDF
+
+
+def Pdf2html(pdf):
+    convertedPDF = convert_func('HTML', './uploads/'+pdf)
+    fileConverted = open('./pdf2html/change_html.html', "wb")
+    fileConverted.write(convertedPDF)
+    fileConverted.close()
+
+
+def Pdf2txt(pdf):
+    convertedPDF = convert('text', './uploads/'+pdf)
+    fileConverted = open('./pdf2txt/text.txt', "w")
+    fileConverted.write(convertedPDF)
+    fileConverted.close()
+
+
+def Pdf2Png(pdf):
+    images = convert_from_path('./uploads/' + pdf)
+    for i, image in enumerate(images):
+        fname = "./pdf2png/image" + str(i) + ".png"
+        image.save(fname, "PNG")
+
+
+def Pdf2Jpg(pdf):
+    images = convert_from_path('./uploads/' + pdf)
+    for i, image in enumerate(images):
+        fname = "./pdf2jpg/image" + str(i) + ".jpg"
+        image.save(fname, "JPEG")
+
+
+@app.route('/convert/upload', methods=['POST'])
+def convert():
+    if request.method == 'POST':
+        file = request.files.get('file')
+        typeToConvert = request.form.get('pdfconverttype')
+        # typeToConvert = 'pdf2jpg'
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        if str(typeToConvert) == 'pdf2jpg':
+            Pdf2Jpg(filename)
+            create_zip('pdf2jpg')
+            zipfilename = 'pdf2jpg.zip'
+            return zipfilename
+
+        if str(typeToConvert) == 'pdf2png':
+            Pdf2Png(filename)
+            create_zip('pdf2png')
+            zipfilename = 'pdf2png.zip'
+            return zipfilename
+
+        if str(typeToConvert) == 'pdf2txt':
+            Pdf2txt(filename)
+            create_zip('pdf2txt')
+            zipfilename = 'pdf2txt.zip'
+            return zipfilename
+
+        if str(typeToConvert) == 'pdf2html':
+            Pdf2html(filename)
+            create_zip('pdf2html')
+            zipfilename = 'pdf2html.zip'
+            return zipfilename
+    return ""
+
+########################################################################################################################
+########################################################################################################################
+########################################################################################################################
+
+
 ########################################################################################################################
 # Merge
 ########################################################################################################################
--- pdfconverter/src/Pages/Merge.js
+++ pdfconverter/src/Pages/Merge.js
@@ -1,9 +1,179 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useMemo, Fragment, useState } from 'react'
+
+import { useDropzone } from 'react-dropzone';
+
+import fileDownload from 'js-file-download';
+
+import { Navbar } from '../Components/Navbar';
+
+const baseStyle = {
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: '70px',
+    borderWidth: 5,
+    borderRadius: 5,
+    borderColor: '#000',
+    borderStyle: 'double',
+    backgroundColor: '#208AF4',
+    color: 'white',
+    outline: 'none',
+    transition: 'border .24s ease-in-out',
+    width: '700px'
+};
+
+const activeStyle = {
+    borderColor: '#2196f3'
+};
+
+const acceptStyle = {
+    borderColor: '#00e676'
+};
+
+const rejectStyle = {
+    borderColor: '#ff1744'
+};
+
 
 export const Merge = () => {
-    return (
-        <div>
+    const [returnFile, setReturnFile] = useState('');
 
-        </div>
+
+    const mergeFile = async e => {
+        try {
+            const res = await axios.get('/merge/combine')
+                .then(res => {
+                    setReturnFile(res.data);
+                    console.log(returnFile)
+                });
+        } catch (err) {
+            console.log('Error in file upload');
+        }
+    }
+    const addFile = async (ind, file) => {
+        const formData = new FormData();
+
+        formData.append('file', file);
+        try {
+            const res = await axios.post('/merge/upload', formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+        } catch (err) {
+            console.log('Error in file upload');
+        }
+    }
+    const onSubmit = async e => {
+        e.preventDefault();
+        console.log(acceptedFiles)
+        for (let i = 0; i < 2; i++) {
+            console.log(acceptedFiles[i])
+            addFile(i, acceptedFiles[i])
+        }
+    }
+
+    const downloadFile = e => {
+        axios.get('/merge/download/' + returnFile, {
+            responseType: 'blob',
+        }).then(res => {
+            console.log(res.data)
+            let rName = returnFile;
+            fileDownload(res.data, rName);
+        });
+    }
+
+
+    const {
+        getRootProps,
+        getInputProps,
+        isDragActive,
+        isDragAccept,
+        isDragReject,
+        acceptedFiles,
+        open
+    } = useDropzone({ accept: '.pdf', noClick: true, noKeyboard: true });
+
+    const style = useMemo(() => ({
+        ...baseStyle,
+        ...(isDragActive ? activeStyle : {}),
+        ...(isDragAccept ? acceptStyle : {}),
+        ...(isDragReject ? rejectStyle : {})
+    }), [
+        isDragActive,
+        isDragReject,
+        isDragAccept
+    ]);
+
+    const filesdata = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size / 1e+6} MB
+        </li>
+    ));
+    return (
+        <>
+            <div className="backImg leading-normal text-indigo-400  bg-cover bg-fixed">
+                <div className="h-screen">
+                    <Navbar />
+                    <div className="w-full">
+
+                        <Fragment>
+                            <div className="container pt-24 md:pt-36 mx-auto flex-row flex-wrap flex-col md:flex-row items-center">
+
+                                <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+
+                                    <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-100 via-pink-400 to-purple-700">
+                                        Merge
+                                    </span>
+
+                                </h1>
+                                <form method="POST">
+                                    <div className="container">
+
+                                        <div {...getRootProps({ style })}>
+                                            <input {...getInputProps()} />
+                                            <p className="my-4 text-3xl md:text-2xl mr-3 text-white opacity-75 font-bold leading-tight text-center md:text-left">Drag and drop some files here</p>
+                                            <div className='uploadbutton'>
+                                                <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                                    type="submit" onClick={onSubmit}>
+                                                    upload
+                                                </button>
+
+                                            </div>
+                                        </div>
+                                        <aside>
+                                            <ul>{filesdata}</ul>
+                                        </aside>
+                                    </div >
+
+                                    <div className="mt-10">
+
+                                        <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                            type="submit" onClick={onSubmit}>
+                                            Upload
+                                        </button>
+                                    </div>
+                                </form>
+                                <div className="mt-10">
+
+                                    <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                        type="submit" onClick={mergeFile}>
+                                        Merge
+                                    </button>
+                                </div>
+                                {returnFile ? (
+                                    <button className="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-3 px-8 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                                        type="submit" onClick={downloadFile}>
+                                        Download
+                                    </button>
+                                ) : null}
+                            </div>
+                        </Fragment>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
