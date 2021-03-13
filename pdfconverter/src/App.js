35333300a1df14e933d2149aadaf8704d628e937
Bhavik-Ardeshna <ardeshnabhavik@gmail.com>
2021-03-13 21:58:15 +0530

    Added Navbar and Home page design

--- pdfconverter/src/App.js
+++ pdfconverter/src/App.js
@@ -4,6 +4,14 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import './App.css';
 import backImg from "./Static/image/header.png";
+import { Buttons } from "./Components/Buttons";
+import { Rotate } from "./Pages/Rotate";
+import { Crop } from "./Pages/Crop";
+import { Merge } from "./Pages/Merge";
+import { Split } from "./Pages/Split";
+import { Convert } from "./Pages/Convert";
+import { Navbar } from "./Components/Navbar";
+
 function App() {
   return (
     <>
@@ -11,27 +19,70 @@ function App() {
         <Switch>
 
           <Route path="/rotate">
-            {/* <Rotate /> */}
+            <Rotate />
           </Route>
 
           <Route path="/crop">
-            {/* <Crop /> */}
+            <Crop />
           </Route>
 
           <Route path="/merge">
-            {/* <Merge /> */}
+            <Merge />
           </Route>
 
           <Route path="/split">
-            {/* <Split /> */}
+            <Split />
           </Route>
 
           <Route path="/convert">
-            {/* <Convert /> */}
+            <Convert />
           </Route>
 
           <Route path="/">
             {/* Home page */}
+
+            <div className="backImg leading-normal text-indigo-400  bg-cover bg-fixed">
+              <div className="h-full">
+                <Navbar />
+
+                <div className="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+                  <div className="flex-row">
+                    <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start ">
+                      <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
+                        Use
+                <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">
+                          PDFConverter
+                </span>
+                to play with PDF!
+              </h1>
+                      <p className="leading-normal text-base md:text-2xl mb-8 text-center md:text-left">
+                        Upload or drop pdf and use PDFConverter features
+              </p>
+                    </div>
+                    <div className="flex ">
+
+                      <Buttons />
+
+                    </div>
+                  </div>
+                  <div className="w-full pt-72 pb-72 text-sm text-center md:text-left fade-in">
+                    <a
+                      className="text-gray-500 no-underline hover:no-underline"
+                      href="#"
+                    >
+                      &copy; App 2021
+              </a>
+              - Template by
+              <a
+                      className="text-gray-500 no-underline hover:no-underline"
+                      href="#"
+                    >
+                      Bhavik@PDFTools
+              </a>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Route>
 
         </Switch>
